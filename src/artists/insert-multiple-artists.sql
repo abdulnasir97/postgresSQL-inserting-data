2630cba6e5a0fc84837551855899d726a396655a
Abdul M Nasir <moeid.nasir@gmail.com>
2023-03-31 17:52:48 -0400

    Add files via upload

---
+++ src/artists/insert-multiple-artists.sql
@@ -0,0 +1,5 @@
+INSERT INTO artists (first_name, last_name, birthday, is_alive)
+VALUES 
+  ('Kehinde', 'Wiley', '1977-02-28', true),
+  ('Pablo', 'Picasso', '1881-10-25', false),
+  ('Vincent', 'van Gogh', '1853-03-30', false);
